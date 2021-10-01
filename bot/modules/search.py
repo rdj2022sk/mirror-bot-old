4a4a84fab9e439d700eab14f905aefa53e30a9b0
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-01 14:16:13 +0530

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -332,5 +332,5 @@ def searchhelp(update, context):
     update.effective_message.reply_photo(IMAGE_URL, help_string, parse_mode=ParseMode.HTML)
     
     
-SEARCHHELP_HANDLER = CommandHandler("tshelp", searchhelp, filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user) & CustomFilters.mirror_owner_filter, run_async=True)
+SEARCHHELP_HANDLER = CommandHandler("tshelp", searchhelp, filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user) & CustomFilters.mirror_owner_filter)
 dispatcher.add_handler(SEARCHHELP_HANDLER)
