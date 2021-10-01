e6bbf7b9c366719d8cc709e92cf220a34def8d04
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-01 14:14:21 +0530

    Update usage.py

--- bot/modules/usage.py
+++ bot/modules/usage.py
@@ -76,6 +76,6 @@ def dyno_usage(update, context):
 
 
 dyno_usage_handler = CommandHandler(command=BotCommands.UsageCommand, callback=dyno_usage,
-                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
                                     
 dispatcher.add_handler(dyno_usage_handler)
